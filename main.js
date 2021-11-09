84a14b1fd3e3824bf6d946dc2ff27550db01d2cf
Tiffany May-Phillips <tiffany.phillips12@gmail.com>
2021-11-09 16:59:37 -0600

    final touches

--- main.js
+++ main.js
@@ -1,56 +1,56 @@
-
-
 function checkout() {
     location.href = "success.html"
 }
 
-// Add items to cart
-let color = {
-    red: 0,
-    white: 0,
-    blue: 0,
-    black: 0,
-    pink: 0
+function lego() {
+    location.href = "https://www.lego.com/en-us"
 }
 
-let sex = {
-    male: 0,
-    female: 0
-}
+// Add items to cart
+let cart = []
 
 function addToCart () {
+    if(document.getElementById('red').checked) {
+       cart.push('Red')
+    } else if(document.getElementById('white').checked) {
+        cart.push('White')
+    } else if(document.getElementById('blue').checked) {
+        cart.push('Blue')
+    } else if(document.getElementById('black').checked) {
+        cart.push('Black')
+    } else if(document.getElementById('pink').checked) {
+        cart.push('Pink')
+    }
+
+    if(document.getElementById('male').checked) {
+        cart.push('Male')
+    } else if(document.getElementById('female').checked) {
+        cart.push('Female')
+    }
+
+    console.log(cart)
+
+    localStorage.setItem("cart", cart);
+    window.location.href="checkout.html";
+}
+
+function getCart () {
+    let input = localStorage.getItem("cart")
+    let cart = input.split(',')
+
     let productColor = 'product-color'
     let productSex = 'product-sex'
     let productDescription = 'Lego Water Bottle'
 
-    if(document.getElementById('red').checked) {
-       color.red++
-       document.getElementById(productColor).innerHTML = `Red ${productDescription}`
-    } else if(document.getElementById('white').checked) {
-        color.white++
-        document.getElementById(productColor).innerHTML = `White ${productDescription}`
-    } else if(document.getElementById('blue').checked) {
-        color.blue++
-        document.getElementById(productColor).innerHTML = `Blue ${productDescription}`
-    } else if(document.getElementById('black').checked) {
-        color.black++
-        document.getElementById(productColor).innerHTML = `Black ${productDescription}`
-    } else if(document.getElementById('pink').checked) {
-        color.pink++
-        document.getElementById(productColor).innerHTML = `Pink ${productDescription}`
-    }
+    console.log(cart)
 
-    if(document.getElementById('male').checked) {
-        sex.male++
-        document.getElementById(productColor).innerHTML = `Male`
-    } else if(document.getElementById('female').checked) {
-         sex.female++
-         document.getElementById(productColor).innerHTML = `Female`
-    }
+    document.getElementById(productColor).innerHTML = `${cart[0]} ${productDescription}`
+    document.getElementById(productSex).innerHTML = `- ${cart[1]}`
+}
 
-    console.log(color, sex)
-    location.href = "checkout.html"
-
+function resetLocalStorage () {
+    let resetValue ={};
+    localStorage.setItem("cart", resetValue);
 }
 
 
