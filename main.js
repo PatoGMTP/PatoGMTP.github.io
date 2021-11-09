d6e2ed9f3e176b8ff9af8c8313c8ea276cf404bf
Tiffany May-Phillips <tiffany.phillips12@gmail.com>
2021-11-09 16:00:32 -0600

    changed dropdown to list

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-function addToCart() {
-    location.href = "checkout.html";
-}
+
 
 function checkout() {
     location.href = "success.html"
 }
+
+// Add items to cart
+let cart = {
+    red: 0,
+    white: 0,
+    blue: 0,
+    black: 0,
+    pink: 0,
+    male: 0,
+    female: 0
+}
+
+function addToCart = () {
+    if(document.getElementById('male').checked) {
+       cart.male++
+    } else if(document.getElementById('female').checked) {
+        cart.female++
+    }
+
+    if(document.getElementById('male').checked) {
+        cart.male++
+    } else if(document.getElementById('female').checked) {
+         cart.female++
+    }
+    
+
+}
+
+console.log()
